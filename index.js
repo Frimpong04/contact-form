7c3cec8a4a5abe4128758772a85df9d9e3411f14
Kwame Frimpong-Boateng <frimpong@Kwames-MacBook-Air.local>
2024-06-13 16:43:30 +0000

    add ids to the error paragraph elements and elements selected via javascript

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-window.alert("JAVASCRIPT IN THE BUILDING");
+// button
+const submitButton = document.getElementById("submit");
+
+// input elements
+// name input elements and value
+ const firstNameInput = document.getElementById("fName").value;
+
+ const lastNameInput = document.getElementById("lName").value;
+
+//  email selector
+ const emailInput = document.getElementById("email").value;
+
+//  textarea input and value
+ const textareaInput = document.getElementById("message").value;
+
+//  radio elements
+const supportRadioInput = document.getElementById("support").value;
+const generalRadioInput = document.getElementById("general").value;
+
+// checked button
+const checkButtonInput = document.getElementById("consent").value;
+
+// add event listeners
+submitButton.addEventListener("onsubmit", () => {
+    console.log(firstNameInput);
+})

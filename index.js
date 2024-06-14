e542638b214362d351f30486c019b16d923edd65
Kwame Frimpong-Boateng <frimpong@Kwames-MacBook-Air.local>
2024-06-14 19:35:09 +0000

    add javascript validation to the form

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// window.alert("Connected to javascript");
 
 
 // form element
@@ -6,21 +7,46 @@ const form = document.querySelector("#form");
 // input elements
 // name input elements and value
  const firstNameInput = document.getElementById("fName");
-
+ firstNameInput.addEventListener("focus", () => {
+    firstNameInput.value = "";
+    firstNameError.textContent = "";
+ })
+ 
  const lastNameInput = document.getElementById("lName");
+
+ lastNameInput.addEventListener("focus", () => {
+    lastNameInput.value = "";
+    lastNameError.textContent = "";
+ })
 
 //  email selector
  const emailInput = document.getElementById("email");
-
+ emailInput.addEventListener("focus", () => {
+    emailInput.value = ""
+    emailError.textContent= "";
+ })
 //  textarea input and value
  const textareaInput = document.getElementById("message");
+ textareaInput.addEventListener("focus", () => {
+    messageError.textContent = "";
+ })
 
 //  radio elements
 const supportRadioInput = document.getElementById("support");
+supportRadioInput.addEventListener("click", () => {
+    queryError.textContent = ""
+});
+
 const generalRadioInput = document.getElementById("general");
+generalRadioInput.addEventListener("click", () => {
+    queryError.textContent = "";
+});
 
 // checked button
-const checkButtonInput = document.getElementById("consent");
+const checkButton= document.getElementById("consent");
+checkButton.addEventListener("click", () => {
+    consentError.textContent = ""
+});
 
 // error elements
 const firstNameError = document.getElementById("fName-error");
@@ -33,11 +59,43 @@ const queryError = document.getElementById("query-error");
 // button element
 const submitButton = document.getElementById("submit");
 
-// form element
-const formElement = document.getElementById("form");
+// event Listenres
 
-// add event listeners
-formElement.addEventListener("onsubmit", (e) => {
-    e.preventDefault();
-    console.log("first");
+
+form.addEventListener("submit", (event) => {
+   event.preventDefault();
+    console.log("submit me");
+
+    if(!firstNameInput.value) {
+        firstNameError.textContent = "I am expecting a first name please";
+    }
+
+    if(!lastNameInput.value) {
+        lastNameError.textContent ="I am expecting your last name please";
+    }
+
+    if(!emailInput.value) {
+        emailError.textContent ="You forget to tnter your email please";
+    }
+
+    if(!emailInput.value.includes("@")) {
+        emailError.textContent = "Enter an email please";
+    }
+
+    if(!supportRadioInput.value || !generalRadioInput.value) {
+        queryError.textContent = "tell us about your enquiry" ;
+    }
+
+    if(!supportRadioInput.checked && !generalRadioInput.checked) {
+        queryError.textContent = "Whta type of enquiry do want to make";
+    }
+
+    if(textareaInput.value === "") {
+        messageError.textContent = "What is your enquiry please?";
+    }
+
+    if(checkButton.checked === false) {
+        consentError.textContent = "kindly give your consent please";
+    }
+
 })
